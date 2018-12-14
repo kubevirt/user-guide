eab9e19f806146c0796246cb551286ae59d3bfd6
Fabian Deutsch <fabiand@fedoraproject.org>
2018-12-14 17:12:31 +0100

    travis: Don't rebuild docs if it was automation
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- push.sh
+++ push.sh
@@ -1,5 +1,7 @@
 set -ex
 [[ -n "${GITHUB_TOKEN}" ]]
+[[ "$(git log -n1 )" =~ Travis ]] && { echo "Commit was authored by Travis, aborting" ; exit 0 ; }
+
 # See https://gist.github.com/willprice/e07efd73fb7f13f917ea
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
